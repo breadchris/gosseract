08742974c45d375e58c9a49f89306aba7250bbe8
Chris <chrislegolife@gmail.com>
2023-08-24 12:43:42 -0700

    Update preprocessflags_x.go

--- preprocessflags_x.go
+++ preprocessflags_x.go
@@ -3,6 +3,6 @@ package gosseract
 // #cgo CXXFLAGS: -std=c++0x
 // #cgo CPPFLAGS: -I/usr/local/include
 // #cgo CPPFLAGS: -Wno-unused-result
-// #cgo darwin LDFLAGS: -L/usr/local/lib -llept -ltesseract
+// #cgo darwin LDFLAGS: -L/usr/local/lib -lleptonica -ltesseract
 // #cgo !darwin LDFLAGS: -L/usr/local/lib -lleptonica -ltesseract
 import "C"
